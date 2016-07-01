0236d258411a06f5e3ad7948bf1a2665ec576e45
Vallabha Rayudu <rayudu.ikkurthi@gmail.com>
2016-07-01 18:54:25 +0530

    Users list and editor table for array examples
    Users list for array examples and Editor table example

---
+++ AureliaSetup/AureliaSetup/src/Users/ListOfUsers.js
@@ -0,0 +1,53 @@
+﻿export  class ListOfUsers{
+
+    constructor()
+    {
+        this.message='List Of Users';
+        this.users=[];
+        this.users.push(new UserLists(0,'Vallabha','80', 'UI Developer','Html5, css3, Javascript','Andhra Pradesh','India',false));
+        this.users.push(new UserLists(1,'Rayudu','70', 'UI Developer','Html5, css3, Javascript','Andhra Pradesh','India',false));
+        this.users.push(new UserLists(2,'Sarath','70', 'Team lead','N/A','Kerala','India',false));
+        this.users.push(new UserLists(3,'Satpathy','90', 'Software Engineer','MVC , .NET, c#','Odisha','India',false));
+        this.users.push(new UserLists(4,'Rohan','75','Product Manager','N/A','Karnataka', 'India',false));
+
+        this.firstName ='';
+        this.designation='';
+        this.technologies='';
+        this.state='';
+        this.country='';
+        this.renderInRightPane(this.users[0]);
+        //this.users[0].ActiveControl = true;
+    }
+
+    renderInRightPane(objUserList)
+    {
+        this.firstName =objUserList.name;
+        this.designation = objUserList.designation;
+        this.technologies = objUserList.technologies;
+        this.state = objUserList.State;
+        this.country = objUserList.Country;
+        this.users[objUserList.index].ActiveControl = true;
+        for(var i=0; i<this.users.length; i++){
+            if(objUserList.index!==i){
+                this.users[i].ActiveControl = false;
+            }
+        }
+        //this.objUserList.ActiveControl(true);
+    }
+}
+
+export class UserLists{
+
+    constructor(index,name,status, designation, technologies,State, Country, ActiveControl )
+    {
+        this.index = index;
+        this.name  = name;
+        this.status = status;
+        this.designation = designation;
+        this.technologies = technologies;
+        this.State = State;
+        this.Country = Country;
+        this.ActiveControl = ActiveControl;
+    }
+}
+
--- AureliaSetup/AureliaSetup/src/app.js
+++ AureliaSetup/AureliaSetup/src/app.js
@@ -4,7 +4,11 @@ export class App {
         config.map([
           { route: ['','welcome'],  name: 'welcome',  moduleId: './welcome',  nav: true, title:'Welcome' },
           { route: 'Bindable',  name: 'Bindable',  moduleId: './Bindable',  nav: true, title:'Bindable Custom Element' },
-          { route: 'repeatFor',  name: 'repeatFor',  moduleId: './repeatFor',  nav: true, title:'RepeatFor' }
+          { route: 'repeatFor',  name: 'repeatFor',  moduleId: './repeatFor',  nav: true, title:'RepeatFor' },
+          { route: 'Users',  name: 'Users',  moduleId: './Users/ListOfUsers',  nav: true, title:'Users' },
+          { route: 'tabelEdit',  name: 'tabelEdit',  moduleId: './Users/tabelEdit',  nav: true, title:'Tabel Editor' }
+
+
         ]);
 
         this.router = router;
